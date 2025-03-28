38372b1e55c4aa5a6b0dae9c31b6871753dc66b9
Anmol100399 <anmolverma0101@email.com>
2025-03-28 07:54:58 -0400

    editing files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://memorable-moments.onrender.com", // Proxy API requests in development
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  }
-})
+});
